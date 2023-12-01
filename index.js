1e9d90435f88a37737811ca29613e0142808cdd4
unknown <eymeric57160@hotmail.fr>
2023-12-01 18:42:30 +0100

    plane work

--- index.js
+++ index.js
@@ -1,96 +1,70 @@
+const plane1 = document.getElementById("plane1");
 const plane = document.getElementById("plane");
+const distanceElement = document.getElementById("distanceEl");
+const speed = 2;
 
 let planeTop = 0;
-const speed = 2;
 let isMovingUp = false;
 let isMovingDown = false;
 let firstLife = false;
-let fa1 = document.getElementById('fa1');
-let fa2 = document.getElementById('fa2');
-let fa3 = document.getElementById('fa3');
-let life = [fa1, fa2, fa3]
-let element = 0
+let fa1 = document.getElementById("fa1");
+let fa2 = document.getElementById("fa2");
+let fa3 = document.getElementById("fa3");
+let life = [fa1, fa2, fa3];
+let element = 0;
+let distance = 1000;
+let traveled = true;
+let counting = true
 
 
 
-function lose() {
-  if(life[1].style.color ==="grey") {
-    loseDiv.style.opacity = "1"
-    loseDiv.style.zIndex = "50"
-  }
-}
+
+/////////////////////////////Plane Move ////////////////////////
+
 function movePlaneUp() {
   plane.style.top = planeTop + "px";
+  
 }
 function movePlaneDown() {
   plane.style.top = planeTop + "px";
 }
-function crash() {
-  const planeRect = plane.getBoundingClientRect();
-  
-  document.querySelectorAll(".cloud").forEach((cloud) => {
-    const cloudRect = cloud.getBoundingClientRect();
-
-    if (!firstLife &&
-      planeRect.left < cloudRect.right &&
-      planeRect.right > cloudRect.left &&
-      planeRect.top < cloudRect.bottom &&
-      planeRect.bottom > cloudRect.top
-    ) {
-      lose();
-    life[element].style.color ="grey"
-    element = (element + 1 ) % life.length;
-    firstLife = true
-    plane.classList.add("lostLife")
-   
-
-    setTimeout(() => {
-     
-      firstLife = false
-      plane.classList.remove("lostLife")
-    },4000)
-   
-    }
-   
-
-  })}
-setInterval(crash, 100);
-
-function update() {
-  if (isMovingUp && planeTop > 0) {
-    planeTop -= speed;
-    movePlaneUp();
-  }
-  if (isMovingDown && planeTop < window.innerHeight - plane.clientHeight) {
-    planeTop += speed;
-    movePlaneDown();
-  }
-  crash()
-  requestAnimationFrame(update);
-}
 
 document.addEventListener("keydown", (e) => {
-  if (e.key === "z") {
+  if (e.key === "z" || e.key === "ArrowUp") {
     isMovingUp = true;
   }
-  if (e.key === "s") {
+  if (e.key === "s" || e.key === "ArrowDown") {
     isMovingDown = true;
   }
+  console.log(e.key);
 });
 
 document.addEventListener("keyup", (e) => {
-  if (e.key === "z") {
+  if (e.key === "z" || e.key === "ArrowUp") {
     isMovingUp = false;
   }
-  if (e.key === "s") {
+  if (e.key === "s" || e.key === "ArrowDown") {
     isMovingDown = false;
   }
 });
 
 
+function update() {
 
+  if (isMovingDown && planeTop < window.innerHeight  - plane.clientHeight - 50) {
+    planeTop += speed;
+    movePlaneDown();
+  }
+  if (isMovingUp && planeTop > 50) {
+    planeTop -= speed;
+    movePlaneUp();
+  }
 
+  crash();
+  requestAnimationFrame(update);
+}
 
+///////////////////////////Cloud Maker ////////////////////////////////
 
 const cloudMaker1 = () => {
   const cloud = document.createElement("span");
@@ -98,7 +72,6 @@ const cloudMaker1 = () => {
 
   const randomX = Math.floor(Math.random() * (window.innerHeight - 50));
   cloud.style.top = randomX + "px";
-
   cloud.style.left = "800px";
   document.body.appendChild(cloud);
 
@@ -111,16 +84,88 @@ const cloudMaker1 = () => {
 
 setInterval(cloudMaker1, 1000);
 
-
-
 update();
 
+//////Distance Traveled /////////////////////////
 
-retry.addEventListener ("click", () => {
+
+
+function DisTraveled() {
+  if (traveled ) {
+    distanceElement.innerText = distance + "m";
+    distance--;
+    distance = distance <= 0 ? 0 : distance - 0;
+
+    if (distance == 0) {
+      win.style.transform = "translateY(00px)";
+      win.style.zIndex = "50";
+      loseDiv.style.display = "none";
+      plane.style.display = "none";
+      localStorage.setItem("lvl1","ok");
+    }
+  }
+}
+
+function stopTraveled() {
+  traveled = false;
+}
+
+setInterval(DisTraveled, 16);
+
+////////////////////////Crash ////////////////////////////////
+
+function crash() {
+  const planeRect = plane1.getBoundingClientRect();
+
+  document.querySelectorAll(".cloud").forEach((cloud) => {
+    const cloudRect = cloud.getBoundingClientRect();
+
+    if (
+      !firstLife &&
+      planeRect.left < cloudRect.right &&
+      planeRect.right > cloudRect.left &&
+      planeRect.top < cloudRect.bottom &&
+      planeRect.bottom > cloudRect.top
+    ) {
+      lose();
+      life[element].style.color = "grey";
+      element = (element + 1) % life.length;
+      firstLife = true;
+      plane.classList.add("lostLife");
+
+      setTimeout(() => {
+        firstLife = false;
+        plane.classList.remove("lostLife");
+      }, 3000);
+    }
+  });
+}
+
+setInterval(crash, 100);
+
+function lose() {
+  if (life[1].style.color === "grey") {
+    loseDiv.style.opacity = "1";
+    loseDiv.style.zIndex = "50";
+    stopTraveled(); 
+  }
+}
+
+/////////////////////Retry/////////////////////
+
+retry.addEventListener("click", () => {
   location.reload();
-})
+});
 
 quit.addEventListener("click", () => {
-  window.location.href='./main.html'
-}
-)
+  window.location.href = "./main.html";
+});
+
+
+
+///////////////////////Win///////////////////////////
+menu.addEventListener("click", () => {
+  window.location.href = "./main.html";
+})
+
+console.log(localStorage);
--- main.js
+++ main.js
@@ -1,25 +1,36 @@
 const cloud = document.getElementsByClassName("span");
-
 const close = document.getElementById("close");
 
+
+
+
+
+
+///////////////////////Cloud Maker //////////////////////////
 const cloudMaker1 = () => {
   const cloud = document.createElement("span");
-  cloud.classList.add("cloud");
-
-  const randomX = Math.floor(Math.random() * (window.innerHeight - 50));
-
   const size = Math.random() * 100 + 100 + "px";
+  const randomX = Math.floor(Math.random() * (window.innerHeight - 50));
+  cloud.classList.add("cloud");
   cloud.style.height = size;
   cloud.style.width = size;
   cloud.style.top = randomX + "px";
-
   document.body.appendChild(cloud);
 
   setTimeout(() => {
     cloud.remove();
-  }, 30000);
+  }, 11000);
 };
 setInterval(cloudMaker1, 1000);
+
+
+
+
+
+///////////////////////////////////////////////////////////
+
+//////////////////////////Cursor/////////////////////////////
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const cursor1 = document.querySelector(".custom-cursor");
@@ -31,11 +42,16 @@ document.addEventListener("DOMContentLoaded", function () {
   lvlContainer.addEventListener("mouseover", () => {
     cursor1.classList.remove("cursor1");
   });
-
+  Rules.addEventListener("mouseover", () => {
+    cursor1.classList.remove("cursor1");
+  });
   main1.addEventListener("mouseout", () => {
     cursor1.classList.add("cursor1");
   });
   lvlContainer.addEventListener("mouseout", () => {
+    cursor1.classList.add("cursor1");
+  });
+  Rules.addEventListener("mouseout", () => {
     cursor1.classList.add("cursor1");
   });
 
@@ -45,19 +61,54 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+
+
+
+play.addEventListener("click", () => {
+  window.location.href = "./index.html";
+});
+
 lvl.addEventListener("click", () => {
   main1.style.display = "none";
   lvlContainer.style.opacity = "5";
-  lvlContainer.style.Zindex = "20";
+  lvlContainer.style.marginTop = "250px";
 });
 
 close.addEventListener("click", () => {
   main1.style.display = "block";
-
   lvlContainer.style.opacity = "-5";
-  lvlContainer.style.Zindex = "-20";
+  lvlContainer.style.marginTop = "-900px";
+});
+
+RulesBtn.addEventListener("click", () => {
+  main1.style.display = "none";
+  Rules.style.marginTop = "220px";
+  Rules.style.display = "block";
+});
+
+closeRules.addEventListener("click", () => {
+  main1.style.display = "block";
+  Rules.style.marginTop = "500px";
+  Rules.style.display = "none";
 });
 
 play.addEventListener("click", () => {
   window.location.href = "./index.html";
 });
+
+
+/////////////////////LocalStorage //////////////
+
+let lvl1Value = localStorage.getItem("lvl1")
+if (lvl1Value) {
+  lvl1.classList.add ("lvlFinished")
+
+}
+
+
+///////////////////lvlSelected//////////////////
+
+lvl1.addEventListener("click", () => {
+  if (lvl1.style.color === "#rgb(0, 170, 255)")
+  window.location.href = "./index.html";
+})
